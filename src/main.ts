966692987104e93c3349599967811aa0e3b6a925
Mani4141 <156489451+Mani4141@users.noreply.github.com>
2025-10-10 09:35:15 +0000

    Added counter, changed styling of page (Step 2 ✅)

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
 import "./style.css";
 
+//button
 const button = document.createElement("button");
 button.id = "clickMe";
 button.textContent = "💎 Click Me!";
 document.body.appendChild(button);
 
+//counter
+let counter: number = 0;
+const counterDiv = document.createElement("div");
+counterDiv.id = "counter";
+counterDiv.textContent = `${counter} 💎`; // fun unit label 💎
+document.body.appendChild(counterDiv);
+
+//counter logic
 button.addEventListener("click", () => {
-  console.log("Button clicked!");
+  counter++;
+  counterDiv.textContent = `${counter} 💎`;
+  console.log(`💎 You now have ${counter} diamonds!`);
 });
